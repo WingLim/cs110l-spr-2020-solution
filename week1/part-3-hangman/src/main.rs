c15c8b35d4224156c5facda2722eec873c04d69a
WingLim <643089849@qq.com>
2021-10-01 11:25:51 +0800

    feat: finish week1 part3

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -33,8 +33,61 @@ fn main() {
     // vector than it is to pull them out of a string. You can get the ith character of
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
+    let chars_len = secret_word_chars.len();
+    let mut user_word_chars: Vec<String> = vec!["-".to_string(); chars_len];
+    let mut guessed_strings: Vec<String> = vec![];
     // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
+    println!("random word: {}", secret_word);
 
     // Your code here! :)
+
+    println!("Welcome to CS110L Hangman!");
+
+    let mut wrong_counter = 0;
+
+    while wrong_counter <= NUM_INCORRECT_GUESSES {
+        print!("Please guess a letter:");
+        io::stdout()
+            .flush()
+            .expect("Error flushing stdout.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+        
+        guessed_strings.push(guess.clone().replace("\n", ""));
+        
+        let guess_char = guess.chars().next().unwrap();
+
+        if secret_word_chars.iter().any(|&x| x == guess_char) {
+            user_word_chars = find_all(&secret_word_chars, &mut user_word_chars, &guess_char);
+        } else {
+            wrong_counter += 1;
+            println!("Sorry, that letter is not in the word");
+        }
+        let joined = user_word_chars.join("");
+        println!("The word so far: {}", joined);
+
+        println!("You have guessed the following letters: {}", guessed_strings.join(""));
+
+        println!("You have guessed {} guesses left", NUM_INCORRECT_GUESSES - wrong_counter);
+
+        if joined == secret_word {
+            println!("Congratulations you guessed the secret word: {}", secret_word);
+            break;
+        }
+        if NUM_INCORRECT_GUESSES - wrong_counter <= 0 {
+            println!("Sorry, you ran out of guesses!");
+            break;
+        }
+    }
+}
+
+fn find_all(a :&Vec<char>, b: &mut Vec<String>, matched: &char) -> Vec<String> {
+    for (idx, val) in a.iter().enumerate() {
+        if val == matched {
+            b[idx] = val.to_string();
+        }
+    }
+    b.to_vec()
 }
