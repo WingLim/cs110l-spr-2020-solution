2f381da02a1370740b1eba5486065aee2af09e77
WingLim <643089849@qq.com>
2021-10-01 15:41:45 +0800

    feat(rwc): finish rwc

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::process;
+use std::fs::File;
+use std::io::{self, BufRead};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,5 +10,30 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    
+    let file = File::open(filename).expect("Invalid file!");
+    let mut lines = Vec::new();
+
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line.expect("Invalid line!");
+        lines.push(line_str)
+    }
+    let lines_count = lines.len();
+
+    let mut words_count = 0;
+    let mut chars_count = 0;
+
+    for line in lines {
+        let splits = line.split_whitespace();
+        for split in splits {
+            words_count += 1;
+            for _ in split.chars() {
+                chars_count += 1;
+            }
+        }
+    }
+
+    println!("Your file has {} lines", lines_count);
+    println!("Your file has {} words", words_count);
+    println!("Your file has {} chars", chars_count);
 }
