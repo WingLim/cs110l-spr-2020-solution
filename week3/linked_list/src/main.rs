63d2e0c28870b043ee46ce83dbc8324957d86eb6
WingLim <643089849@qq.com>
2021-10-01 22:12:32 +0800

    feat(linked_list): test in main function

--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -2,21 +2,40 @@ use linked_list::LinkedList;
 pub mod linked_list;
 
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
+    let mut list: LinkedList<String> = LinkedList::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
-    for i in 1..12 {
-        list.push_front(i);
+    for i in vec!["a", "b", "c", "d"] {
+        list.push_front(i.to_string());
     }
+
+    // test generics
+    println!("===== test generics =====");
     println!("{}", list);
     println!("list size: {}", list.get_size());
     println!("top element: {}", list.pop_front().unwrap());
     println!("{}", list);
     println!("size: {}", list.get_size());
-    println!("{}", list.to_string()); // ToString impl for anything impl Display
+    println!("");
 
-    // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    // test Clone trait
+    println!("===== test clone trait =====");
+    let mut list_clone = list.clone();
+    list_clone.push_front("e".to_string());
+    println!("original: {}", list);
+    println!("cloned: {}", list_clone);
+    println!("");
+
+    // test Iterator
+    println!("===== test iterator trait =====");
+    for val in &list {
+       println!("{}", val);
+    }
+    println!("");
+
+    // test PartialEq trait
+    println!("===== test clone trait =====");
+    list.push_front("e".to_string());
+    println!("orginal == cloned: {}", list == list_clone);
+    
 }
