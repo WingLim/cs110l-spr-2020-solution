649c5d15fa0110b391eac5e2731f0c31ff3b93fa
WingLim <643089849@qq.com>
2021-10-03 19:54:40 +0800

    feat(deet): finish milestone1
    run inferior, print child process status when stopped, exited and signaled

--- proj-1/deet/src/debugger.rs
+++ proj-1/deet/src/debugger.rs
@@ -1,5 +1,5 @@
 use crate::debugger_command::DebuggerCommand;
-use crate::inferior::Inferior;
+use crate::inferior::{Inferior, Status};
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
@@ -35,9 +35,12 @@ impl Debugger {
                     if let Some(inferior) = Inferior::new(&self.target, &args) {
                         // Create the inferior
                         self.inferior = Some(inferior);
-                        // TODO (milestone 1): make the inferior run
-                        // You may use self.inferior.as_mut().unwrap() to get a mutable reference
-                        // to the Inferior object
+                        
+                        match self.inferior.as_mut().unwrap().continue_run().unwrap() {
+                            Status::Stopped(signal, _rip) => println!("Child stopped (signal {})", signal),
+                            Status::Exited(exit_code) => println!("Child exited (status {})", exit_code),
+                            Status::Signaled(signal) => println!("Child exited (signal {})", signal),
+                        }
                     } else {
                         println!("Error starting subprocess");
                     }
--- proj-1/deet/src/inferior.rs
+++ proj-1/deet/src/inferior.rs
@@ -2,7 +2,8 @@ use nix::sys::ptrace;
 use nix::sys::signal;
 use nix::sys::wait::{waitpid, WaitPidFlag, WaitStatus};
 use nix::unistd::Pid;
-use std::process::Child;
+use std::os::unix::prelude::CommandExt;
+use std::process::{Child, Command};
 
 pub enum Status {
     /// Indicates inferior stopped. Contains the signal that stopped the process, as well as the
@@ -34,12 +35,15 @@ impl Inferior {
     /// Attempts to start a new inferior process. Returns Some(Inferior) if successful, or None if
     /// an error is encountered.
     pub fn new(target: &str, args: &Vec<String>) -> Option<Inferior> {
-        // TODO: implement me!
-        println!(
-            "Inferior::new not implemented! target={}, args={:?}",
-            target, args
-        );
-        None
+        let mut cmd = Command::new(target);
+        cmd.args(args);
+        unsafe {
+            cmd.pre_exec(child_traceme);
+        }
+        let child = cmd.spawn().ok()?;
+        let inferior = Inferior {child};
+
+        Some(inferior)
     }
 
     /// Returns the pid of this inferior.
@@ -59,5 +63,10 @@ impl Inferior {
             }
             other => panic!("waitpid returned unexpected status: {:?}", other),
         })
+    }
+
+    pub fn continue_run(&self) -> Result<Status, nix::Error> {
+        ptrace::cont(self.pid(), None)?;
+        self.wait(None)
     }
 }
